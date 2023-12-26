c9c99e41663889691b39776ceda0966ecdaf4640
Ahmet Uygun <ahmetuygun@Ahmets-MacBook-Pro.local>
2023-12-26 18:41:45 +0000

    xx

--- src/main/java/com/company/originmaster/OriginMasterApplication.java
+++ src/main/java/com/company/originmaster/OriginMasterApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OriginMasterApplication {
-
+ ///erere
     public static void main(String[] args) {
         SpringApplication.run(OriginMasterApplication.class, args);
     }
