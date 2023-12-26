63ce6a0b2e219a09880942dbc0504343cd5fc75c
Ahmet Uygun <ahmetuygun@Ahmets-MacBook-Pro.local>
2023-12-26 18:28:46 +0000

    initial commit

---
+++ src/main/java/com/company/originmaster/OriginMasterApplication.java
@@ -0,0 +1,13 @@
+package com.company.originmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriginMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OriginMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/originmaster/OriginMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.originmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
